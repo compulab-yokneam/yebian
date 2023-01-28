54c6dd16385b483b88fe5b5859c1d7946b2bd2eb
Valentin Raevsky <valentin@compulab.co.il>
2023-01-28 22:43:48 +0200

    Update bootstrap.sh
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -x
 
- git clone https://github.com/compulab-yokneam/yebian-tools.git ../yebian-tools
- git -C ../yebian-tools checkout devel-next 
+bootstrap=$(realpath ${BASH_SOURCE[0]})
+base_dir=$(dirname $(dirname ${bootstrap}))/yebian-tools
+BRANCH=devel-next
+SRC_URI=https://github.com/compulab-yokneam/yebian-tools.git
+
+if [[ -d ${base_dir}/.git ]];then
+	git -C ${base_dir} remote update
+	git -C ${base_dir} pull
+else
+	git clone -b ${BRANCH} ${SRC_URI} ${base_dir}
+fi
